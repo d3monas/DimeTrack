d3f8b5cea37ad4d9d0d602c9c0866862d1e9e170
d3monas <marciusmes@gmail.com>
2026-07-20 18:50:41 +0300

    feat: add new colors

--- lib/consts.ts
+++ lib/consts.ts
@@ -21,3 +21,12 @@ export const DEFAULT_CATEGORY_ICON = "Tag"
 export const DEFAULT_ACCOUNT_ICON = "Wallet"
 
 export const STARTING_BALANCE_CATEGORY = "Starting Balance"
+
+export const ACCENT_COLORS = [
+    {name: "Slate", hex: "#18181b"},
+    {name: "Emerald", hex: "#10b981"},
+    {name: "Ocean", hex: "#0ea5e9"},
+    {name: "Royal", hex: "#8b5cf6"},
+    {name: "Sunset", hex: "#f97316"},
+    {name: "Rose", hex: "#f43f5e"},
+]
