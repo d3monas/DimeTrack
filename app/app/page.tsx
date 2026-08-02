90d1ee4d8ab4091ff831d2d92a9e58a7f7a00cfa
d3monas <marciusmes@gmail.com>
2026-08-02 11:51:23 +0300

    feat: update props

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -957,6 +957,7 @@ export default function Home() {
                 categories={categories}
                 accounts={accounts} 
                 onAddTransaction={() => setOpen(true)} 
+                categoryCustomization={categoryCustomization}
               />
             </div>
           </TabsContent>
