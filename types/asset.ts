3d38e7e4d7ebd16032940643b737026b57bf8e73
d3monas <marciusmes@gmail.com>
2026-08-08 23:53:33 +0300

    feat: update asset type

--- types/asset.ts
+++ types/asset.ts
@@ -1,7 +1,19 @@
+export type InvestmentType = "Stock" | "ETF" | "Crypto" | "Other"
+
+export type InvestmentTransaction = {
+  id: string
+  type: "buy" | "sell" | "update"
+  date: string
+  quantity: number
+  pricePerUnit: number
+  notes?: string
+}
+
 export type Asset = {
   id: string
   name: string
-  value: number
+  ticker?: string
+  type: InvestmentType
   notes?: string
-  isRecurring?: boolean
+  transactions: InvestmentTransaction[]
 }
