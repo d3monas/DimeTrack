18361eeb57c265ad54974e90e364164ad053a664
d3monas <marciusmes@gmail.com>
2026-08-11 12:15:50 +0300

    feat: create new dashboard customization type

---
+++ types/dashboard.ts
@@ -0,0 +1,10 @@
+export type DashboardVisibility = {
+    networth: boolean
+    networth_history: boolean
+    upcoming: boolean
+    smart_stats: boolean
+    trend: boolean
+    forecast: boolean
+    accounts: boolean
+    breakdown: boolean
+}
